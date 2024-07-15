df11833998c659d17a44a8d41e7e2a6449066a2f
Bob Tanner <tanner@real-time.com>
2024-07-15 21:42:16 +0000

    Renamed create-table to create-tables

--- .devcontainer/docker-entrypoint-initdb.d/001-create-tables.sql
+++ .devcontainer/docker-entrypoint-initdb.d/001-create-tables.sql
@@ -1,6 +1,10 @@
 USE metaserver;
+CREATE TABLE `blacklist` (
+	  `entry` NOT NULL auto_increment,
+	  `hostname` varchar(80),
+	  PRIMARY KEY  (`entry`))
 CREATE TABLE `servers` (
-	  `entry` int(11) NOT NULL auto_increment,
+	  `entry` NOT NULL auto_increment,
 	  `hostname` varchar(80),
 	  `port` int(11),
 	  `html_comment` varchar(1024),
@@ -9,10 +13,10 @@ CREATE TABLE `servers` (
 	  `mapbase` varchar(64),
 	  `codebase` varchar(64),
 	  `flags` varchar(20),
-	  `num_players` int(11),
-	  `in_bytes` int(11),
-	  `out_bytes` int(11),
-	  `uptime` int(11),
+	  `num_players` int,
+	  `in_bytes` int,
+	  `out_bytes` int,
+	  `uptime` int,
 	  `version` varchar(64),
 	  `sc_version` varchar(20),
 	  `cs_version` varchar(20),
