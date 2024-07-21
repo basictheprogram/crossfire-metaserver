b292775855a8e0277426f53ec8b190f2ea36bdcf
Bob Tanner <tanner@real-time.com>
2024-07-21 16:25:42 +0000

    Add Docker Compose, Docker ignore file, GitHub Actions for Docker publishing, and update Certbot environment variables
    - **.devcontainer/docker-compose.yml**:
      - Added `depends_on` directive for the `mysql` service.
    
    - **.dockerignore**:
      - Created to exclude unnecessary files and directories from Docker build context.
    
    - **.github/workflows/docker-publish.yml**:
      - Added GitHub Actions workflow to build and publish Docker images on push and pull request events to the `vscode-devcontainer` branch.
    
    - **docker-compose.yml**:
      - Added services for `mysql`, `www`, and `certbot` with appropriate configurations and dependencies.
    
    - **docker/certbot/.env** and **docker/certbot/.env.sample**:
      - Updated environment variable names to `CERTBOT_EMAIL` and `CERTBOT_DOMAIN`.
    
    - **docker/certbot/certify-init.sh**:
      - Refactored script to use new Certbot environment variables.
    
    - **html/meta_client.php**:
      - Minor code refactoring for better readability and formatting.

--- docker/certbot/certify-init.sh
+++ docker/certbot/certify-init.sh
@@ -2,35 +2,29 @@
 
 set -e
 
-if [ -z $DOMAIN ]; then
+if [ -z $CERTBOT_DOMAIN ]; then
     echo "==> Warning: DOMAIN variable is null"
     exit 1
 fi
 
-if [ -z $EMAIL ]; then
+if [ -z $CERTBOT_EMAIL ]; then
     echo "==> Warning: EMAIL variable is null"
     exit 1
 fi
 
-# until nc -z proxy 80; do
-#    echo "==> Waiting for proxy to start"
-#    sleep 1s & wait ${!}
-# done
-
-echo "==> Getting certificate for ${DOMAIN}"
+echo "==> Getting certificate for ${CERTBOT_DOMAIN}"
 
 certbot --verbose certonly \
     --webroot \
-    --webroot-path "/usr/local/apache2/htdocs/" \
-    --domain "${DOMAIN}" \
-    --domain "www.${DOMAIN}" \
-    --email ${EMAIL} \
+    --webroot-path "/var/www/html/" \
+    --domain "${CERTBOT_DOMAIN}" \
+    --email "${CERTBOT_EMAIL}" \
     --rsa-key-size 4096 \
     --agree-tos \
     --non-interactive \
     --no-eff-email
 
 # Check if DH params file exists, if not, generate it
-if [ ! -f "/etc/letsencrypt/live/www.savysocials.com/ssl-dhparams.pem" ]; then
-    openssl dhparam -out /etc/letsencrypt/live/www.savysocials.com/ssl-dhparams.pem 2048
+if [ ! -f "/etc/letsencrypt/live/www.${CERTBOT_DOMAIN}/ssl-dhparams.pem" ]; then
+    openssl dhparam -out /etc/letsencrypt/live/www.${CERTBOT_DOMAIN}/ssl-dhparams.pem 2048
 fi
